138627d57c6c5b902ca7e803c733801e9a768b40
LElTEDEV <zinenizi@gmail.com>
2025-07-11 08:28:28 -0300

    first test

--- src/use-cases/register-use-case.spec.ts
+++ src/use-cases/register-use-case.spec.ts
@@ -1,5 +1,37 @@
-import { expect, test } from "vitest";
+import { compare } from "bcryptjs";
+import { expect, describe, it } from "vitest";
 
-test("check if it works", () => {
-  expect(2 + 2).toBe(4);
+import { RegisterUseCase } from "./register-use-case";
+
+describe("Register Use Case", () => {
+  it("should hash user password upon registration", async () => {
+    const registerUseCase = new RegisterUseCase({
+      async create(data) {
+        return {
+          id: "user-1",
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        };
+      },
+
+      async findByEmail(email) {
+        return null;
+      },
+    });
+
+    const { user } = await registerUseCase.execute({
+      name: "John Doe",
+      email: "johndoe@gmail.com",
+      password: "123123",
+    });
+
+    const isPasswordCorrectlyHashed = await compare(
+      "123123",
+      user.password_hash
+    );
+
+    expect(isPasswordCorrectlyHashed).toBe(true);
+  });
 });
