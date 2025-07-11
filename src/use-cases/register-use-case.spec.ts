cb6a4ede6e6fb541ffeda8e739b48b8ee30ffee1
LElTEDEV <zinenizi@gmail.com>
2025-07-11 08:35:11 -0300

    created in-memory-users-repository

--- src/use-cases/register-use-case.spec.ts
+++ src/use-cases/register-use-case.spec.ts
@@ -2,24 +2,12 @@ import { compare } from "bcryptjs";
 import { expect, describe, it } from "vitest";
 
 import { RegisterUseCase } from "./register-use-case";
+import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
 
 describe("Register Use Case", () => {
   it("should hash user password upon registration", async () => {
-    const registerUseCase = new RegisterUseCase({
-      async create(data) {
-        return {
-          id: "user-1",
-          name: data.name,
-          email: data.email,
-          password_hash: data.password_hash,
-          created_at: new Date(),
-        };
-      },
-
-      async findByEmail(email) {
-        return null;
-      },
-    });
+    const inMemoryUsersRepository = new InMemoryUsersRepository();
+    const registerUseCase = new RegisterUseCase(inMemoryUsersRepository);
 
     const { user } = await registerUseCase.execute({
       name: "John Doe",
