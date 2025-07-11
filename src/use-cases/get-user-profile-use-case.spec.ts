702d700f975f5b71982cee4628933cd55e83a230
LElTEDEV <zinenizi@gmail.com>
2025-07-11 13:14:39 -0300

    test: created get-user-profile test

---
+++ src/use-cases/get-user-profile-use-case.spec.ts
@@ -0,0 +1,35 @@
+import { expect, describe, it, beforeEach } from "vitest";
+
+import { GetUserProfileUseCase } from "./get-user-profile-use-case";
+import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
+import { hash } from "bcryptjs";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+
+let inMemoryRepository: InMemoryUsersRepository;
+let sut: GetUserProfileUseCase;
+
+describe("Get User Profile Use Case", () => {
+  beforeEach(() => {
+    inMemoryRepository = new InMemoryUsersRepository();
+    sut = new GetUserProfileUseCase(inMemoryRepository);
+  });
+
+  it("should be able get user profile", async () => {
+    const createdUser = await inMemoryRepository.create({
+      name: "John Doe",
+      email: "jhondoe@gmail.com",
+      password_hash: await hash("123123", 6),
+    });
+
+    const { user } = await sut.execute({ userId: createdUser.id });
+
+    expect(user.id).toEqual(expect.any(String));
+    expect(user.name).toEqual("John Doe");
+  });
+
+  it("should not be able get user profile with wrong id", async () => {
+    expect(async () => {
+      await sut.execute({ userId: "user-0202" });
+    }).rejects.instanceOf(ResourceNotFoundError);
+  });
+});
