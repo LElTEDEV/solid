ba856112bede35b1b6f5c228e0a7840e971059eb
LElTEDEV <zinenizi@gmail.com>
2025-07-14 09:19:06 -0300

    feat: created check-in test and use case

---
+++ src/use-cases/check-in-use-case.ts
@@ -0,0 +1,27 @@
+import { CheckInsRespository } from "@/repositories/check-ins-repository";
+import { CheckIn } from "@prisma/client";
+
+interface CheckInUseCaseRequest {
+  userId: string;
+  gymId: string;
+}
+
+interface CheckInUseCaseResponse {
+  checkIn: CheckIn;
+}
+
+export class CheckInUseCase {
+  constructor(private checkInsRepository: CheckInsRespository) {}
+
+  async execute({
+    gymId,
+    userId,
+  }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const checkIn = await this.checkInsRepository.create({
+      gym_id: gymId,
+      user_id: userId,
+    });
+
+    return { checkIn };
+  }
+}
