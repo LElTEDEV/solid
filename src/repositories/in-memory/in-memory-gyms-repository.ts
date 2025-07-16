7fbc202ac1f0bbcdf6510f557efce3774f5b43be
LElTEDEV <zinenizi@gmail.com>
2025-07-16 09:52:37 -0300

    feat: created searchMany method

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -13,6 +13,12 @@ export class InMemoryGymsRepository implements GymsRepository {
     return gym;
   }
 
+  async searchMany(query: string, page: number) {
+    return this.gyms
+      .filter((item) => item.title.toLowerCase().includes(query.toLowerCase()))
+      .slice((page - 1) * 20, page * 20);
+  }
+
   async create(data: Prisma.GymCreateInput) {
     const gym = {
       id: data.id ?? randomUUID(),
